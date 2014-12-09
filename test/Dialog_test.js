9967f694a7bc0308f4f3b04befa2a4dca677b3c4
Jan Kaluza <jkaluza@redhat.com>
2014-12-09 19:37:02 +0100

    Move common stuff into TestCommon.js

--- test/Dialog_test.js
+++ test/Dialog_test.js
@@ -1,27 +1,4 @@
-var jsdom = require("jsdom");
-var canvas = require('canvas');
-global.Image = canvas.Image;
-
-Main.WIDTH = 1024; Main.HEIGHT = 672; Main.CENTER_X = Main.WIDTH / 2; Main.CENTER_Y = Main.HEIGHT / 2;
-function Main() {}
-global.Main = Main;
-
-if (typeof localStorage === "undefined" || localStorage === null) {
-  var LocalStorage = require('node-localstorage').LocalStorage;
-  localStorage = new LocalStorage('./scratch');
-}
-
-window = jsdom.jsdom('<html><head></head><body><div id="rondavu_container"></div></body></html>')
-global.window = window.parentWindow;
-global.document = window.document;
-
-global.PIXI = require("../bower_components/pixi.js/bin/pixi.js")
-global.Ship = require("../js/Ship.js");
-global.Item = require("../js/Item.js");
-global.ItemSprite = require("../js/ItemSprite.js");
-global.ItemManager = require("../js/ItemManager.js");
-global.Inventory = require("../js/Inventory.js");
-global.Dialog = require("../js/Dialog.js");
+require("./TestCommon.js").prepareTest();
 
 function hasText(dialog, text) {
     for (var i = 0; i < dialog.children.length; i++) {
