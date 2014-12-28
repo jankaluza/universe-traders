a47d9b0f35e4f72805cf2ff021cd101f5fc0d846
Jan Kaluza <jkaluza@redhat.com>
2014-12-28 12:25:27 +0100

    Show ships above the planets

--- js/ObjectManager.js
+++ js/ObjectManager.js
@@ -113,7 +113,7 @@ ObjectManager.prototype.addToStage = function(obj) {
         this.staged[this.staged.length] = obj;
         obj.position.x = -(obj.mapX - this.left) * Universe.MAP_POINT_SIZE;
         obj.position.y = -(obj.mapY - this.top) * Universe.MAP_POINT_SIZE;
-        this.universe.addChildAt(obj, 1);
+        this.universe.addChildAt(obj, this.universe.children.length - 1);
         return;
     }
 
